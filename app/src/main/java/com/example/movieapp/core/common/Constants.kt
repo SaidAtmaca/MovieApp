bd56f7ff246b3fabf77718f949dd28bbb76430a6
saidatmaca <beyoglu98@hotmail.com>
2024-11-18 11:10:10 +0300

    feat: Image loading add.

--- app/src/main/java/com/example/movieapp/core/common/Constants.kt
+++ app/src/main/java/com/example/movieapp/core/common/Constants.kt
@@ -8,8 +8,7 @@ object Constants {
 
     const val API_KEY = "d5d7846d9e8c6cf2c07f58ef3b9149b1"
     const val API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNWQ3ODQ2ZDllOGM2Y2YyYzA3ZjU4ZWYzYjkxNDliMSIsIm5iZiI6MTczMTc2MjI4MC44NTcxMDQ4LCJzdWIiOiI2NzM4OTdhMGZmMGUxODM3M2NlZWJkYjUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.tETdO1k8e-i7RHYaPFOb-9gWioLd6Xtgksj1QdMpeSY"
-
-
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
 
 }
--- app/src/main/java/com/example/movieapp/presentation/components/MovieCard.kt
+++ app/src/main/java/com/example/movieapp/presentation/components/MovieCard.kt
@@ -1,23 +1,31 @@
 package com.example.movieapp.presentation.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.movieapp.core.common.Constants
 import com.example.movieapp.data.model.MovieOverViewModel
 import com.example.movieapp.presentation.ui.theme.CornerRound
 
@@ -26,6 +34,7 @@ import com.example.movieapp.presentation.ui.theme.CornerRound
 fun MovieCard(model:MovieOverViewModel,
               onMovieClicked : (MovieOverViewModel)->Unit) {
 
+    val url = Constants.IMAGE_BASE_URL+model.backdropPath
     Card (
         modifier = Modifier
             .fillMaxWidth()
@@ -39,16 +48,47 @@ fun MovieCard(model:MovieOverViewModel,
         border = BorderStroke(0.5.dp,Color.Gray)
 
     ){
-        Column(
-            modifier = Modifier.fillMaxSize().padding(5.dp),
-            verticalArrangement = Arrangement.Center
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
 
-            Text( text = model.originalTitle,
-                style = MaterialTheme.typography.bodyMedium)
+            ImageBox(url)
 
-            Text(text = model.releaseDate,
-                style = MaterialTheme.typography.bodySmall)
+            Column(
+                modifier = Modifier.fillMaxSize().weight(1f).padding(5.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Text( text = model.originalTitle,
+                    style = MaterialTheme.typography.bodyMedium)
+
+                Text(text = model.releaseDate,
+                    style = MaterialTheme.typography.bodySmall)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ImageBox(url: String) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        val painter = rememberAsyncImagePainter(url)
+
+        Image(
+            painter = painter,
+            contentDescription = "Loaded Image",
+            modifier = Modifier.size(50.dp)
+        )
+
+
+        // Yükleniyor göstergesi
+        if (painter.state is coil.compose.AsyncImagePainter.State.Loading) {
+            CircularProgressIndicator(color = Color.Gray)
         }
     }
+
 }
